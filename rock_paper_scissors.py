39138a16e79c8d5e9732cd3cde82ac5b598ae6ac
andrei-tihan <andreitihan@yahoo.com>
2025-07-27 17:49:43 -0600

    Rewrote basic structure for looping user input

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,10 +1,15 @@
 import random
 
 hand = ["rock", "paper", "scissors"]
-player = input("Will you play rock, paper, or scissors?")
 computer = random.choice(hand)
 
-if player.lower() in hand:
-    print("Input test")
-else:
-    print("Please enter rock, paper, or scissors.")
+while True:
+    player = input("Will you play rock, paper, or scissors? Type quit to exit game.")
+
+    if player.lower() in hand:
+        print("Input test")
+    elif player.lower() == "quit":
+        print("Game over, thanks for playing!")
+        break
+    else:
+        print("Invalid input. Please enter rock, paper, or scissors. Type quit to exit game.")
